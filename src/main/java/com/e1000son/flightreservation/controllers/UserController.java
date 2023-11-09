ded32cd53c14df8417ab10a559cf13b9c7b8e1e7
e1000son <emilsonvontade@gmail.com>
2023-11-09 04:09:36 +0200

    Register and Login pages

--- src/main/java/com/e1000son/flightreservation/controllers/UserController.java
+++ src/main/java/com/e1000son/flightreservation/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -18,9 +19,21 @@ public class UserController {
         return "login/registerUser";
     }
 
-    @RequestMapping(value = "registerUser", method = RequestMethod.POST)
+    @RequestMapping(value = "/registerUser", method = RequestMethod.POST)
     public String saveUser(@ModelAttribute("user") User user, ModelMap modelMap){
         userRepository.save(user);
+        return "/login/login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestParam("email") String email, @RequestParam("password") String password,
+    ModelMap modelMap){
+        User user = userRepository.findByEmail(email);
+        if (user.getPassword().equals(password)){
+            return "findFlights";
+        } else {
+            modelMap.addAttribute("msg", "Invalid username or password. Please, try again.");
+        }
         return "/login/login";
     }
 
--- src/main/java/com/e1000son/flightreservation/repos/IUserRepository.java
+++ src/main/java/com/e1000son/flightreservation/repos/IUserRepository.java
@@ -4,4 +4,5 @@ import com.e1000son.flightreservation.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IUserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
