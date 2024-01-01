38d90df669df86e71d1e3c0c7adb9586204d1990
e1000son <emilsonvontade@gmail.com>
2024-01-01 19:18:56 +0200

    Implementing INFO Logging for Flight Reservation

--- src/main/java/com/e1000son/flightreservation/controllers/FlightController.java
+++ src/main/java/com/e1000son/flightreservation/controllers/FlightController.java
@@ -2,6 +2,8 @@ package com.e1000son.flightreservation.controllers;
 
 import com.e1000son.flightreservation.entities.Flight;
 import com.e1000son.flightreservation.repos.IFlightRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -16,12 +18,16 @@ import java.util.List;
 public class FlightController {
     @Autowired
     private IFlightRepository flightRepository;
+    private static final Logger LOGGER = LoggerFactory.getLogger(FlightController.class);
     @RequestMapping("/findFlights")
     public String findFlights(@RequestParam("from") String from, @RequestParam("to") String to,
                               @RequestParam("departureDate") @DateTimeFormat(pattern = "dd-MM-yyyy") Date departureDate,
                               ModelMap modelMap){
+        LOGGER.info("Inside findFlights(). From: {from}. To: {to}. Departure date: {departureDate}");
         List<Flight> flights = flightRepository.findFlights(from, to, departureDate);
         modelMap.addAttribute("flights", flights);
+
+        LOGGER.info("Flights found are: " + flights);
 
         return "displayFlights";
     }
--- src/main/java/com/e1000son/flightreservation/controllers/ReservationController.java
+++ src/main/java/com/e1000son/flightreservation/controllers/ReservationController.java
@@ -5,6 +5,8 @@ import com.e1000son.flightreservation.entities.Flight;
 import com.e1000son.flightreservation.entities.Reservation;
 import com.e1000son.flightreservation.repos.IFlightRepository;
 import com.e1000son.flightreservation.services.ReservationServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -18,16 +20,20 @@ public class ReservationController {
     private IFlightRepository flightRepository;
     @Autowired
     private ReservationServiceImpl reservationService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReservationController.class);
 
     @RequestMapping("/showCompleteReservation")
     public String showCompleteReservation(@RequestParam("flightId") Long flightId, ModelMap modelMap){
+        LOGGER.info("Inside showCompleteReservation(). Invoked with the flight ID: " + flightId);
         Flight flight = flightRepository.findById(flightId).get();
         modelMap.addAttribute("flight", flight);
+        LOGGER.info("Flight found are: " + flight);
         return "completeReservation";
     }
 
     @RequestMapping(value = "/completeReservation", method = RequestMethod.POST)
     public String completeReservation(ReservationRequest request, ModelMap modelMap){
+        LOGGER.info("Inside completeReservation() " + request);
 
         Reservation reservation = reservationService.bookFlight(request);
         modelMap.addAttribute("msg", "Reservation created successifully (ID: " +
