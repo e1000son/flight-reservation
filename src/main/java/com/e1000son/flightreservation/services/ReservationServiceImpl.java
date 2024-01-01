60beb908a16c9e5cb82c1de61c892a09b15ee88f
e1000son <emilsonvontade@gmail.com>
2024-01-01 19:39:32 +0200

    Implementing INFO Logging for Services Layer

--- src/main/java/com/e1000son/flightreservation/services/ReservationServiceImpl.java
+++ src/main/java/com/e1000son/flightreservation/services/ReservationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.e1000son.flightreservation.services;
 
+import com.e1000son.flightreservation.controllers.ReservationController;
 import com.e1000son.flightreservation.dto.ReservationRequest;
 import com.e1000son.flightreservation.entities.Flight;
 import com.e1000son.flightreservation.entities.Passenger;
@@ -9,6 +10,8 @@ import com.e1000son.flightreservation.repos.IPassengerRepository;
 import com.e1000son.flightreservation.repos.IReservationRepository;
 import com.e1000son.flightreservation.util.EmailUtil;
 import com.e1000son.flightreservation.util.PDFGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,8 +29,10 @@ public class ReservationServiceImpl implements IReservationService{
     private PDFGenerator pdfGenerator;
     @Autowired
     private EmailUtil emailUtil;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReservationServiceImpl.class);
     @Override
     public Reservation bookFlight(ReservationRequest reservationRequest) {
+        LOGGER.info("Inside bookFlight()");
 
         //In reality the payment would be done here
         //reservationRequest.getCardNumber();
@@ -44,6 +49,7 @@ public class ReservationServiceImpl implements IReservationService{
         passenger.setEmail(reservationRequest.getPassengerEmail());
         passenger.setPhone(reservationRequest.getPassengerPhone());
         Passenger savedPassenger = passengerRepository.save(passenger);
+        LOGGER.info("Saved Passenger: " + savedPassenger);
 
         Reservation reservation = new Reservation();
         reservation.setFlight(flight);
@@ -51,9 +57,12 @@ public class ReservationServiceImpl implements IReservationService{
         reservation.setCheckedIn(false);
         reservation.setCreated(new Date());
         Reservation savedReservation = reservationRepository.save(reservation);
+        LOGGER.info("Saved Reservation: " + savedReservation);
 
         String filePath = "/Users/e1000son/Documents/reservas/reserva" + savedReservation.getId() + ".pdf";
+        LOGGER.info("Generating Intinerary: " + filePath);
         pdfGenerator.generateItinerary(savedReservation, filePath);
+        LOGGER.info("Emailing the itinerary...");
         emailUtil.sendItinerary(passenger.getEmail(), filePath);
 
         return savedReservation;
